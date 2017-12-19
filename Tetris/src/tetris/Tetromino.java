1cddd029aa5475d49e4d251a10fb7220fdeb62c8
Mswig <34383729+Mswig@users.noreply.github.com>
2017-12-18 22:46:17 -0600

    Update Tetromino.java

--- Tetris/src/tetris/Tetromino.java
+++ Tetris/src/tetris/Tetromino.java
@@ -1,14 +1,32 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tetris;
 
 /**
- *
- * @author mvstu
+ * Class for creating tetris blocks: tetriminos.  
+ * @author Mswig
+ * @since 12/18/2017
+ * @see Tetris.java
  */
 public class Tetromino {
+    
+    /*
+    array that stores the shape of the block, used for creating new shapes
+    */
+    public boolean[][] layout;
+    
+    /*
+    integer value that stores the color of the block
+    */
+    public int color;
+    
+    public void tetromino(){
+        layout = new boolean[5][5];
+        color = 1;
+    }
+    
+    public void tetromino(int shape){
+        switch(shape){
+            case 1:  layout = {
+        }
+    }
     
 }
