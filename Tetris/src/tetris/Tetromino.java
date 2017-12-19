63b07df3ff3c5bbebd72c053e9f8351bd3f308de
Mswig <34383729+Mswig@users.noreply.github.com>
2017-12-18 23:09:36 -0600

    Update Tetromino.java

--- Tetris/src/tetris/Tetromino.java
+++ Tetris/src/tetris/Tetromino.java
@@ -1,4 +1,6 @@
 package tetris;
+import java.awt.Color;
+import javax.swing.*;
 
 /**
  * Class for creating tetris blocks: tetriminos.  
@@ -16,16 +18,58 @@ public class Tetromino {
     /*
     integer value that stores the color of the block
     */
-    public int color;
+    public Color color;
     
     public void tetromino(){
-        layout = new boolean[5][5];
-        color = 1;
+        layout = new boolean[4][2];
+        color = new Color(0, 0, 0);
     }
     
     public void tetromino(int shape){
         switch(shape){
-            case 1:  layout = {
+            // "I" piece
+            case 1: layout = new boolean[][] { 
+                                              {false, false, false, false},
+                                              {true, true, true, true},
+                                              };
+                    color = Color.CYAN;
+            // "J" piece
+            case 2: layout = new boolean[][] { 
+                                              {false, true, false, false},
+                                              {false, true, true, true},
+                                              };
+                    color = Color.BLUE;
+            // "L" piece
+            case 3: layout = new boolean[][] { 
+                                              {false, false, false, true},
+                                              {false, true, true, true},
+                                              };
+                    color = Color.ORANGE;
+            // "O" piece
+            case 4: layout = new boolean[][] { 
+                                              {false, true, true, false},
+                                              {false, true, true, false},
+                                              };
+                    color = Color.YELLOW;
+            // "S" piece
+            case 5: layout = new boolean[][] { 
+                                              {false, true, true, false},
+                                              {true, true, false, false},
+                                              };
+                    color = Color.GREEN;
+            // "T" piece
+            case 6: layout = new boolean[][] { 
+                                              {false, false, true, false},
+                                              {false, true, true, true},
+                                              };
+                    color = Color.MAGENTA;
+            // "Z" piece
+            case 7: layout = new boolean[][] { 
+                                              {true, true, false, false},
+                                              {false, true, true, false},
+                                              };
+                    color = Color.RED;       
+            default: tetromino();
         }
     }
     
